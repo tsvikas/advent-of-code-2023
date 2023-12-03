d3c981787e96c5752693309a394787f5bd1e5c7a
Tsvika S <tsvika@github>
2023-12-03 20:57:41 +0200

    optimize finding gears

--- aoc2023/d3b.py
+++ aoc2023/d3b.py
@@ -18,9 +18,13 @@ def extract_gear_ratios(engine_schematic: list[str]):
             x1, x2 = match.span()
             value = int(match.group())
             for y in range(max(0, y0 - 1), min(y0 + 2, len(engine_schematic))):
-                for x in range(max(0, x1 - 1), min(x2 + 1, len(engine_schematic[y]))):
-                    if engine_schematic[y][x] == "*":
-                        gears[y, x].append(value)
+                slice_start = max(0, x1 - 1)
+                slice_end = min(x2 + 1, len(engine_schematic[y]))
+                line_slice = engine_schematic[y][slice_start:slice_end]
+                if "*" in line_slice:
+                    x = line_slice.index("*") + slice_start
+                    gears[y, x].append(value)
+                    break
     gear_ratios = []
     expected_number_of_values = 2
     for location, gear_values in gears.items():
