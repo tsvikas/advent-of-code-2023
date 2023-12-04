da1f913c4a1ffb5e2f6e074dbd8089ff90d5f0fc
Tsvika S <tsvika@github>
2023-12-04 13:57:11 +0200

    import function from before

--- aoc2023/d4b.py
+++ aoc2023/d4b.py
@@ -1,18 +1,6 @@
 from aocd import data
 
-from aoc2023.d4a import TEST_INPUTS  # noqa: F401
-
-
-def score_card(line: str) -> int:
-    """
-    >>> [score_card(line) for line in TEST_INPUTS]
-    [4, 2, 2, 1, 0, 0]
-    """
-    numbers = line.split(":")[1]
-    winning_numbers = [int(s) for s in numbers.split("|")[0].split()]
-    card_numbers = [int(s) for s in numbers.split("|")[1].split()]
-    num_wins = sum(n in winning_numbers for n in card_numbers)
-    return num_wins
+from aoc2023.d4a import TEST_INPUTS, card_wins_count  # noqa: F401
 
 
 def get_cards_count(lines: list[str]) -> list[int]:
@@ -23,7 +11,7 @@ def get_cards_count(lines: list[str]) -> list[int]:
     cards_count = [1] * len(lines)
     for card_idx, card_count in enumerate(cards_count):
         card = lines[card_idx]
-        num_wins = score_card(card)
+        num_wins = card_wins_count(card)
         for card_won in range(card_idx + 1, card_idx + num_wins + 1):
             cards_count[card_won] += card_count
     return cards_count
