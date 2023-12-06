7d4ce3eb9b67400d72192d3aaaa2445413551447
Tsvika S <tsvika@github>
2023-12-06 17:38:59 +0200

    solve d6

---
+++ aoc2023/d6a.py
@@ -0,0 +1,62 @@
+import math
+from dataclasses import dataclass
+
+from aocd import data
+
+TEST_INPUT = """\
+Time:      7  15   30
+Distance:  9  40  200
+"""
+
+
+@dataclass
+class Race:
+    time: int
+    distance: int
+
+    def n_ways_to_beat(self) -> int:
+        """
+        >>> Race(7, 9).n_ways_to_beat()
+        4
+        >>> Race(15, 40).n_ways_to_beat()
+        8
+        >>> Race(30, 200).n_ways_to_beat()
+        9
+        """
+        # if we hold for t0, we got v = t0, x = v * dt = t0 (t-t0)
+        # we win if t0 (t-t0) > d
+        # so, t0^2 - t0*t + d < 0
+        # so, (t - sqrt(t^2 - 4d))/2 < t0 < (t + sqrt(t^2 - 4d))/2
+        # so, floor((t - sqrt(t^2 - 4d))/2) < t0 < ceil((t + sqrt(t^2 - 4d))/2)
+        mantisa = math.sqrt(self.time**2 - 4 * self.distance)
+        left = math.floor((self.time - mantisa) / 2)
+        right = math.ceil((self.time + mantisa) / 2)
+        return right - left - 1
+
+
+def parse_lines(lines: list[str]) -> list[Race]:
+    """
+    >>> parse_lines(TEST_INPUT.splitlines())
+    [Race(time=7, distance=9), Race(time=15, distance=40), Race(time=30, distance=200)]
+    """
+    times = [int(x) for x in lines[0].split()[1:]]
+    distances = [int(x) for x in lines[1].split()[1:]]
+    return [Race(t, d) for t, d in zip(times, distances, strict=True)]
+
+
+def process_lines(lines: list[str]) -> int:
+    """
+    >>> process_lines(TEST_INPUT.splitlines())
+    288
+    """
+    return math.prod(race.n_ways_to_beat() for race in parse_lines(lines))
+
+
+def main() -> int:
+    lines = data.splitlines()
+    result = process_lines(lines)
+    return result
+
+
+if __name__ == "__main__":
+    print(main())
---
+++ aoc2023/d6b.py
@@ -0,0 +1,31 @@
+from aocd import data
+
+from aoc2023.d6a import TEST_INPUT, Race  # noqa: F401
+
+
+def parse_lines(lines: list[str]) -> Race:
+    """
+    >>> parse_lines(TEST_INPUT.splitlines())
+    Race(time=71530, distance=940200)
+    """
+    times = int("".join(lines[0].split()[1:]))
+    distances = int("".join(lines[1].split()[1:]))
+    return Race(times, distances)
+
+
+def process_lines(lines: list[str]) -> int:
+    """
+    >>> process_lines(TEST_INPUT.splitlines())
+    71503
+    """
+    return parse_lines(lines).n_ways_to_beat()
+
+
+def main() -> int:
+    lines = data.splitlines()
+    result = process_lines(lines)
+    return result
+
+
+if __name__ == "__main__":
+    print(main())
