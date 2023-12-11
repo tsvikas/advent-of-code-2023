7fc02dea254d306f51677766c5a99b4686d028c5
Tsvika S <tsvika@github>
2023-12-11 13:21:45 +0200

    solve d11

---
+++ aoc2023/d11a.py
@@ -0,0 +1,101 @@
+import itertools
+from collections.abc import Generator, Iterable
+
+from aocd import data
+
+TEST_INPUT = """\
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+"""
+
+TEST_INPUT_EXPENDED = """\
+....#........
+.........#...
+#............
+.............
+.............
+........#....
+.#...........
+............#
+.............
+.............
+.........#...
+#....#.......
+"""
+
+
+def expand_grid(grid: list[str]) -> list[str]:
+    """
+    >>> expand_grid(TEST_INPUT.splitlines()) == TEST_INPUT_EXPENDED.splitlines()
+    True
+    """
+    for _ in range(2):
+        grid = [
+            "".join(grid[j][i] for j in range(len(grid))) for i in range(len(grid[0]))
+        ]
+        new_grid = []
+        for line in grid:
+            new_grid.append(line)
+            if line == "." * len(line):
+                new_grid.append("." * len(line))
+        grid = new_grid
+    return grid
+
+
+def extract_galaxies(grid: list[str]) -> Generator[tuple[int, int], None, None]:
+    """
+    >>> list(extract_galaxies(TEST_INPUT.splitlines()))
+    [(0, 3), (1, 7), (2, 0), (4, 6), (5, 1), (6, 9), (8, 7), (9, 0), (9, 4)]
+    """
+    for i, line in enumerate(grid):
+        for j, char in enumerate(line):
+            if char == "#":
+                yield i, j
+
+
+def distance_galaxies(
+    galaxies: Iterable[tuple[int, int]]
+) -> Generator[int, None, None]:
+    """
+    >>> list(distance_galaxies(extract_galaxies(TEST_INPUT.splitlines())))[:5]
+    [5, 5, 7, 7, 12]
+    """
+    for g1, g2 in itertools.combinations(galaxies, 2):
+        yield abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
+
+
+def distance_galaxies_on_map(lines: list[str]) -> list[int]:
+    """
+    >>> distance_galaxies_on_map(TEST_INPUT.splitlines())[:5]
+    [6, 6, 9, 9, 15]
+    """
+    grid = expand_grid(lines)
+    galaxies = extract_galaxies(grid)
+    distances = distance_galaxies(galaxies)
+    return list(distances)
+
+
+def process_lines(lines: list[str]) -> int:
+    """
+    >>> process_lines(TEST_INPUT.splitlines())
+    374
+    """
+    return sum(distance_galaxies_on_map(lines))
+
+
+def main() -> int:
+    lines = data.splitlines()
+    result = process_lines(lines)
+    return result
+
+
+if __name__ == "__main__":
+    print(main())
---
+++ aoc2023/d11b.py
@@ -0,0 +1,89 @@
+import itertools
+from collections.abc import Generator, Iterable
+
+from aocd import data
+
+from aoc2023.d11a import (
+    TEST_INPUT,  # noqa: F401
+    extract_galaxies,
+)
+
+
+def find_empty_rows_and_cols(grid: list[str]) -> tuple[list[int], list[int]]:
+    """
+    >>> find_empty_rows_and_cols(TEST_INPUT.splitlines())
+    ([3, 7], [2, 5, 8])
+    """
+    empty_rows = [i for i, line in enumerate(grid) if line == "." * len(line)]
+    empty_cols = [
+        i for i in range(len(grid[0])) if all(line[i] == "." for line in grid)
+    ]
+    return empty_rows, empty_cols
+
+
+def distance_galaxies(
+    galaxies: Iterable[tuple[int, int]],
+    empty_rows: list[int] | None = None,
+    empty_cols: list[int] | None = None,
+    expansion_factor: int = 1,
+) -> Generator[int, None, None]:
+    """
+    >>> galaxies = list(extract_galaxies(TEST_INPUT.splitlines()))
+    >>> empty_rows, empty_cols = find_empty_rows_and_cols(TEST_INPUT.splitlines())
+    >>> list(distance_galaxies(galaxies))[:5]
+    [5, 5, 7, 7, 12]
+    >>> list(distance_galaxies(galaxies, empty_rows, empty_cols, 2))[:5]
+    [6, 6, 9, 9, 15]
+    >>> list(distance_galaxies(galaxies, empty_rows, empty_cols, 10))[:5]
+    [14, 14, 25, 25, 39]
+    """
+    empty_rows = empty_rows or []
+    empty_cols = empty_cols or []
+
+    for g1, g2 in itertools.combinations(galaxies, 2):
+        dist = abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
+        empty_rows_in_between = [
+            i for i in empty_rows if min(g1[0], g2[0]) < i < max(g1[0], g2[0])
+        ]
+        empty_cols_in_between = [
+            i for i in empty_cols if min(g1[1], g2[1]) < i < max(g1[1], g2[1])
+        ]
+        dist_expanded = dist + (
+            len(empty_rows_in_between) + len(empty_cols_in_between)
+        ) * (expansion_factor - 1)
+        yield dist_expanded
+
+
+def distance_galaxies_on_map(lines: list[str], expansion_factor: int) -> list[int]:
+    """
+    >>> distance_galaxies_on_map(TEST_INPUT.splitlines(), 10)[:5]
+    [14, 14, 25, 25, 39]
+    """
+    empty_rows, empty_cols = find_empty_rows_and_cols(lines)
+    galaxies = extract_galaxies(lines)
+    distances = distance_galaxies(
+        galaxies, empty_rows, empty_cols, expansion_factor=expansion_factor
+    )
+    return list(distances)
+
+
+def process_lines(lines: list[str], expansion_factor: int) -> int:
+    """
+    >>> process_lines(TEST_INPUT.splitlines(), 2)
+    374
+    >>> process_lines(TEST_INPUT.splitlines(), 10)
+    1030
+    >>> process_lines(TEST_INPUT.splitlines(), 100)
+    8410
+    """
+    return sum(distance_galaxies_on_map(lines, expansion_factor))
+
+
+def main() -> int:
+    lines = data.splitlines()
+    result = process_lines(lines, 1000000)
+    return result
+
+
+if __name__ == "__main__":
+    print(main())
