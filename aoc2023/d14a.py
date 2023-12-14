f7655c992a6717f8a7cf07a1fc78df89acee42db
Tsvika S <tsvika@github>
2023-12-14 15:20:02 +0200

    use class

--- aoc2023/d14a.py
+++ aoc2023/d14a.py
@@ -1,3 +1,8 @@
+from dataclasses import dataclass
+from typing import Self
+
+import numpy as np
+import numpy.typing as npt
 from aocd import data
 
 TEST_INPUT = """\
@@ -13,40 +18,39 @@ O.#..O.#.#
 #OO..#....
 """
 
-TEST_INPUT_TILTED = """\
-OOOO.#.O..
-OO..#....#
-OO..O##..O
-O..#.OO...
-........#.
-..#....#.#
-..O..#.O.O
-..O.......
-#....###..
-#....#....
-"""
 
+@dataclass
+class RocksMap:
+    array: npt.NDArray[np.string_]
 
-def total_load(space: list[str]) -> int:
-    """
-    >>> total_load(TEST_INPUT.splitlines())
-    136
-    """
-    load = 0
-    for column in zip(*space, strict=True):
-        next_load = len(column)
-        for row, char in enumerate(column):
-            match char:
-                case "#":
-                    next_load = len(column) - 1 - row
-                case "O":
-                    load += next_load
-                    next_load -= 1
-                case ".":
-                    pass
-                case _:
-                    raise ValueError(f"illegal character: {char}")
-    return load
+    def __str__(self) -> str:
+        return "\n".join("".join(row) for row in self.array)
+
+    @classmethod
+    def from_lines(cls, lines: list[str]) -> Self:
+        return cls(np.array([list(line) for line in lines]))
+
+    def total_load_north(self) -> int:
+        """
+        >>> rocks = RocksMap.from_lines(TEST_INPUT.splitlines())
+        >>> rocks.total_load_north()
+        136
+        """
+        load = 0
+        for column in self.array.transpose():
+            next_load = len(column)
+            for row, char in enumerate(column):
+                match char:
+                    case "#":
+                        next_load = len(column) - 1 - row
+                    case "O":
+                        load += next_load
+                        next_load -= 1
+                    case ".":
+                        pass
+                    case _:
+                        raise ValueError(f"illegal character: {char}")
+        return load
 
 
 def process_lines(lines: list[str]) -> int:
@@ -54,7 +58,7 @@ def process_lines(lines: list[str]) -> int:
     >>> process_lines(TEST_INPUT.splitlines())
     136
     """
-    return total_load(lines)
+    return RocksMap.from_lines(lines).total_load_north()
 
 
 def main() -> int:
