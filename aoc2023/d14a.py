fe5451c109d5a568de792de9c25195c309a46428
Tsvika S <tsvika@github>
2023-12-14 16:53:13 +0200

    use class in 14a

--- aoc2023/d14a.py
+++ aoc2023/d14a.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from dataclasses import dataclass
 from typing import Self
 
@@ -19,12 +20,89 @@ O.#..O.#.#
 """
 
 
+def tilt_line(line: Iterable[str]) -> list[str]:
+    """
+    >>> ''.join(tilt_line('O....#....'))
+    'O....#....'
+    >>> ''.join(tilt_line('O.OO#....#'))
+    'OOO.#....#'
+    >>> ''.join(tilt_line('.....##...'))
+    '.....##...'
+    >>> ''.join(tilt_line('OO.#O....O'))
+    'OO.#OO....'
+    """
+    new_line: list[str] = []
+    new_section_rocks = 0
+    new_section_spaces = 0
+    for c in line:
+        match c:
+            case "O":
+                new_section_rocks += 1
+            case ".":
+                new_section_spaces += 1
+            case "#":
+                new_line += (
+                    ["O"] * new_section_rocks + ["."] * new_section_spaces + ["#"]
+                )
+                new_section_rocks = 0
+                new_section_spaces = 0
+            case _:
+                raise ValueError(f"illegal character {c!r}")
+    new_line += ["O"] * new_section_rocks + ["."] * new_section_spaces
+    return new_line
+
+
 @dataclass
 class RocksMap:
     array: npt.NDArray[np.string_]
 
     def __str__(self) -> str:
         return "\n".join("".join(row) for row in self.array)
+
+    def transpose(self) -> Self:
+        """
+        >>> rocks = RocksMap.from_lines(TEST_INPUT.splitlines())
+        >>> print(rocks.transpose())
+        OO.O.O..##
+        ...OO....O
+        .O...#O..O
+        .O.#......
+        .#.O......
+        #.#..O#.##
+        ..#...O.#.
+        ....O#.O#.
+        ....#.....
+        .#.O.#O...
+        """
+        return self.__class__(self.array.T)
+
+    def tilt_west(self) -> Self:
+        return self.__class__(np.array([tilt_line(line) for line in self.array]))
+
+    def tilt_north(self) -> Self:
+        """
+        >>> rocks = RocksMap.from_lines(TEST_INPUT.splitlines())
+        >>> print(rocks.tilt_north())
+        OOOO.#.O..
+        OO..#....#
+        OO..O##..O
+        O..#.OO...
+        ........#.
+        ..#....#.#
+        ..O..#.O.O
+        ..O.......
+        #....###..
+        #....#....
+        """
+        return self.transpose().tilt_west().transpose()
+
+    def tilt_east(self) -> Self:
+        return self.__class__(
+            np.array([tilt_line(line[::-1])[::-1] for line in self.array])
+        )
+
+    def tilt_south(self) -> Self:
+        return self.transpose().tilt_east().transpose()
 
     @classmethod
     def from_lines(cls, lines: list[str]) -> Self:
@@ -33,24 +111,15 @@ class RocksMap:
     def total_load_north(self) -> int:
         """
         >>> rocks = RocksMap.from_lines(TEST_INPUT.splitlines())
-        >>> rocks.total_load_north()
+        >>> rocks.tilt_north().total_load_north()
         136
         """
-        load = 0
-        for column in self.array.transpose():
-            next_load = len(column)
-            for row, char in enumerate(column):
-                match char:
-                    case "#":
-                        next_load = len(column) - 1 - row
-                    case "O":
-                        load += next_load
-                        next_load -= 1
-                    case ".":
-                        pass
-                    case _:
-                        raise ValueError(f"illegal character: {char}")
-        return load
+        return sum(
+            len(column) - row
+            for column in self.array.transpose()
+            for row, char in enumerate(column)
+            if char == "O"
+        )
 
 
 def process_lines(lines: list[str]) -> int:
@@ -58,7 +127,7 @@ def process_lines(lines: list[str]) -> int:
     >>> process_lines(TEST_INPUT.splitlines())
     136
     """
-    return RocksMap.from_lines(lines).total_load_north()
+    return RocksMap.from_lines(lines).tilt_north().total_load_north()
 
 
 def main() -> int:
