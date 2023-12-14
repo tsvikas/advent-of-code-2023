58459ec1c535a6fcdc9d32f0f1c329de52971c06
Tsvika S <tsvika@github>
2023-12-14 17:31:47 +0200

    solve 14b

---
+++ aoc2023/d14b.py
@@ -0,0 +1,79 @@
+from aocd import data
+
+from aoc2023.d14a import (
+    TEST_INPUT,  # noqa: F401
+    RocksMap,
+)
+
+
+def cycle(rocks: RocksMap, n: int) -> RocksMap:
+    """
+    >>> rocks = RocksMap.from_lines(TEST_INPUT.splitlines())
+    >>> print("|", cycle(rocks, 1))
+    | .....#....
+    ....#...O#
+    ...OO##...
+    .OO#......
+    .....OOO#.
+    .O#...O#.#
+    ....O#....
+    ......OOOO
+    #...O###..
+    #..OO#....
+    >>> print("|", cycle(rocks, 2))
+    | .....#....
+    ....#...O#
+    .....##...
+    ..O#......
+    .....OOO#.
+    .O#...O#.#
+    ....O#...O
+    .......OOO
+    #..OO###..
+    #.OOO#...O
+    >>> print("|", cycle(rocks, 3))
+    | .....#....
+    ....#...O#
+    .....##...
+    ..O#......
+    .....OOO#.
+    .O#...O#.#
+    ....O#...O
+    .......OOO
+    #...O###.O
+    #.OOO#...O
+    """
+    cache = {rocks.array.tobytes(): 0}
+    cycle_length = i = None
+    for i in range(1, n + 1):
+        rocks = rocks.tilt_north().tilt_west().tilt_south().tilt_east()
+        rocks_b = rocks.array.tobytes()
+        if rocks_b in cache:
+            cycle_start = cache[rocks_b]
+            cycle_length = i - cycle_start
+            break
+        cache[rocks_b] = i
+    if cycle_length is not None:
+        while (n - i) % cycle_length != 0:
+            i += 1
+            rocks = rocks.tilt_north().tilt_west().tilt_south().tilt_east()
+    return rocks
+
+
+def process_lines(lines: list[str]) -> int:
+    """
+    >>> process_lines(TEST_INPUT.splitlines())
+    64
+    """
+    rocks = RocksMap.from_lines(lines)
+    return cycle(rocks, 1_000_000_000).total_load_north()
+
+
+def main() -> int:
+    lines = data.splitlines()
+    result = process_lines(lines)
+    return result
+
+
+if __name__ == "__main__":
+    print(main())
