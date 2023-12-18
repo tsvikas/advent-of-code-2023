08c6f4edcb2603ddd4a0ceb207657274a43e6e8f
Tsvika S <tsvika@github>
2023-12-18 20:42:31 +0200

    fix mypy for d16

--- aoc2023/d16b.py
+++ aoc2023/d16b.py
@@ -1,5 +1,5 @@
 from aocd import data
-from joblib.parallel import Parallel, delayed
+from joblib.parallel import Parallel, delayed  # type: ignore[import-untyped]
 
 from aoc2023.d16a import TEST_INPUT, MirrorGrid  # noqa: F401
 from aoc2023.grid import Point
@@ -18,7 +18,7 @@ def max_energized(line: str) -> int:
         + [(Point(y, max_x - 1), Point(0, -1)) for y in range(max_y)]
         + [(Point(max_y - 1, x), Point(-1, 0)) for x in range(max_x)]
     )
-    beam_energies = Parallel(n_jobs=-1, verbose=0)(
+    beam_energies: list[int] = Parallel(n_jobs=-1, verbose=0)(
         delayed(mirrors.beam_energy)(*init_position) for init_position in init_positions
     )
     return max(beam_energies)
