a0885c04b14cf6b9a248c4434d4d4b52aa30ea76
Tsvika S <tsvika@github>
2023-12-18 20:58:24 +0200

    solve d17b

---
+++ aoc2023/d17b.py
@@ -0,0 +1,31 @@
+from aocd import data
+
+from aoc2023.d17a import TEST_INPUT, HeatGrid  # noqa: F401
+
+TEST_INPUT_2 = """\
+111111111111
+999999999991
+999999999991
+999999999991
+999999999991
+"""
+
+
+def process_lines(lines: str) -> int:
+    """
+    >>> process_lines(TEST_INPUT)
+    94
+    >>> process_lines(TEST_INPUT_2)
+    71
+    """
+    return HeatGrid.from_line(lines).least_heat_loss(4, 10)
+
+
+def main() -> int:
+    lines = data
+    result = process_lines(lines)
+    return result
+
+
+if __name__ == "__main__":
+    print(main())
