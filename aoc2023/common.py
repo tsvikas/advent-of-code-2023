8d11eeb7c822acc1b6c1eee2a31f97302fd0fc7b
Tsvika S <tsvika@github>
2024-01-03 22:13:06 +0200

    solve 21a

--- aoc2023/common.py
+++ aoc2023/common.py
@@ -1,8 +1,11 @@
 from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Self
+from typing import Any, Self
 
 from aocd import get_data, submit  # type: ignore[attr-defined]
+
+SolveInput = str | tuple[Any, ...]
+SolveFunc = Callable[[Any], int]
 
 
 @dataclass
@@ -10,12 +13,15 @@ class Solution:
     year: int
     day: int
     part: str
-    process_lines: Callable[[str], int]
-    tests: dict[str, int]
+    process_lines: SolveFunc
+    tests: dict[SolveInput, int]
 
     @classmethod
     def from_file(
-        cls, filename: str, process_lines: Callable[[str], int], tests: dict[str, int]
+        cls,
+        filename: str,
+        process_lines: SolveFunc,
+        tests: dict[SolveInput, int],
     ) -> Self:
         year = int(filename.split("/")[-2][-4:])
         day = int(filename.split("/")[-1].split(".")[0][1:-1])
@@ -24,7 +30,10 @@ class Solution:
 
     def test_inputs(self) -> None:
         for i, (test, expected) in enumerate(self.tests.items()):
-            actual = self.process_lines(test)
+            if isinstance(test, tuple):
+                actual = self.process_lines(*test)
+            else:
+                actual = self.process_lines(test)
             assert actual == expected, f"{i}: {actual=} {expected=}"
 
     def solve(self) -> int:
---
+++ aoc2023/d21a.py
@@ -0,0 +1,66 @@
+from aoc2023.common import Solution
+from aoc2023.grid import Point
+
+TEST_INPUT = """\
+...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........
+"""
+
+
+def map_to_locations(lines: str) -> tuple[Point, set[Point]]:
+    valid_locations = set()
+    start = None
+    for y, line in enumerate(lines.splitlines()):
+        for x, char in enumerate(line):
+            if char == "S":
+                start = Point(y, x)
+            if char in ["S", "."]:
+                valid_locations.add(Point(y, x))
+    assert start is not None
+    return start, valid_locations
+
+
+def step(start_locations: set[Point], valid_locations: set[Point]) -> set[Point]:
+    end_locations = set()
+    for location in start_locations:
+        for new_location in [
+            location.up(),
+            location.down(),
+            location.left(),
+            location.right(),
+        ]:
+            if new_location in valid_locations:
+                end_locations.add(new_location)
+    return end_locations
+
+
+def steps(start: Point, valid_locations: set[Point], n: int) -> set[Point]:
+    locations = {start}
+    for _ in range(n):
+        locations = step(locations, valid_locations)
+    return locations
+
+
+def count_reachable(start: Point, valid_locations: set[Point], n: int) -> int:
+    return len(steps(start, valid_locations, n))
+
+
+def process_lines(lines: str, n: int = 64) -> int:
+    start, valid_locations = map_to_locations(lines)
+    return count_reachable(start, valid_locations, n)
+
+
+solution = Solution.from_file(__file__, process_lines, {(TEST_INPUT, 6): 16})
+
+if __name__ == "__main__":
+    solution.test_inputs()
+    solution.submit()
