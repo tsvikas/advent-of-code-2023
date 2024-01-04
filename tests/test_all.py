ff210ea352c1b614d7e99e689de4c98d47799e21
Tsvika S <tsvika@github>
2024-01-04 03:09:02 +0200

    test inputs on tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,8 +1,12 @@
 import importlib
 import tomllib
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import pytest
+
+if TYPE_CHECKING:
+    from aoc2023.common import Solution
 
 SOLUTIONS = tomllib.loads(
     Path(__file__).parent.joinpath("my_solutions.toml").read_text()
@@ -12,4 +16,6 @@ SOLUTIONS = tomllib.loads(
 @pytest.mark.parametrize(("name", "expected"), list(SOLUTIONS.items()))
 def test_all(name: str, expected: int):
     module = importlib.import_module(f"aoc2023.{name}")
-    assert module.solution.solve() == expected
+    solution: Solution = module.solution
+    solution.test_inputs()
+    assert solution.solve() == expected
