67e80966e3fb1bb6b3b5928c6dd8eace759c803b
Tsvika S <tsvika@github>
2024-01-04 05:09:25 +0200

    improve d21

--- aoc2023/d21b.py
+++ aoc2023/d21b.py
@@ -1,6 +1,7 @@
 from aoc2023.common import Solution
-from aoc2023.d21a import count_reachable, map_to_locations
+from aoc2023.d21a import map_to_locations, step
 from aoc2023.d21a import process_lines as process_lines_a
+from aoc2023.grid import Point
 
 TEST_INPUT = """\
 ...........
@@ -33,6 +34,31 @@ def expand_map(data: str, n: int) -> str:
 TEST_INPUT_EXPANDED = expand_map(TEST_INPUT, 5)
 
 
+def count_reachable(
+    start: Point, valid_locations: set[Point], ns: list[int]
+) -> list[int]:
+    assert start in valid_locations
+    assert ns == sorted(ns)
+    last_new_locations: set[Point] = {start}
+    last_old_locations: set[Point] = set()
+    previous_locations: set[Point] = set()
+    reachable = []
+    for n in range(ns[-1]):
+        if n in ns:
+            reachable.append(len(last_new_locations | last_old_locations))
+        last_new_locations, last_old_locations, previous_locations = step(
+            last_new_locations, last_old_locations, previous_locations, valid_locations
+        )
+    reachable.append(len(last_new_locations | last_old_locations))
+    return reachable
+
+
+def solve_quadratic_series(series: list[int], k: int) -> int:
+    d1, d2 = series[1] - series[0], series[2] - series[1]
+    dd1 = d2 - d1
+    return series[0] + d1 * k + dd1 * (k * (k - 1) // 2)
+
+
 def count_infinite_reachable(lines: str, n: int) -> int:
     lines_ = lines.splitlines()
     size_y = len(lines_)
@@ -45,12 +71,10 @@ def count_infinite_reachable(lines: str, n: int) -> int:
     assert n == st + k * size_y
     expanded_lines = expand_map(lines, 4)
     start, valid_locations = map_to_locations(expanded_lines)
-    reachable = [
-        count_reachable(start, valid_locations, st + i * size_y) for i in range(3)
-    ]
-    d1, d2 = reachable[1] - reachable[0], reachable[2] - reachable[1]
-    dd1 = d2 - d1
-    return reachable[0] + d1 * k + dd1 * (k * (k - 1) // 2)
+    reachable = count_reachable(
+        start, valid_locations, [st + i * size_y for i in range(3)]
+    )
+    return solve_quadratic_series(reachable, k)
 
 
 def process_lines(lines: str, n: int = 26501365) -> int:
